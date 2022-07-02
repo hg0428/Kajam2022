f48ed9ee1953fc7a6ffd25afd4d4f950bb403ef8
hg0428 <hudson.gouge@icloud.com>
2022-07-02 19:46:14 -0400

    Added a story

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 'use strict';
 let goaway = document.getElementById('goaway');
+let goaway2 = document.getElementById('goaway2');
 goaway.onclick = () => {
   document.getElementById('instructions').style.display = 'none';
+}
+goaway2.onclick = () => {
+  document.getElementById('story').style.display = 'none';
 }
 var ground = null;
 const game = new Game();
 window.onscroll = function() {
   window.scrollTo(0, 0);
 }
+game.loadSprite('e2l', 'e2l.png');
+game.loadSprite('e2r', 'e2r.png');
 game.loadSprite('enemyl', 'enemy-left.png');
 game.loadSprite('chest', 'chest.png');
 game.loadSprite('sugar-cane', 'sugar-cane.png');
@@ -18,13 +24,16 @@ const healthbar = document.getElementById('health-bar');
 const actionBtn = document.getElementById('action-btn');
 let Enemies = [];
 class Enemy {
-  constructor(x, bottom, width, height, health = 100) {
+  constructor(x, bottom, width, height, health = 100, left='enemyl', right='enemyr') {
+    console.log(left, right);
+    this.left = left;
+    this.right = right;
     let thing = this.thing = new game.Thing({
       x,
       bottom,
       width,
       height,
-      background: Sprite('enemyl'),
+      background: Sprite(this.left),
       custom: {
         physics: true,
         pass: true,
@@ -32,11 +41,12 @@ class Enemy {
       }
     });
     this._health = health;
+    const self = this;
     thing.when('draw', () => {
       if (thing.vel.x > 0) {
-        thing.colorScheme = { draw: Sprite('enemyr') };
+        thing.colorScheme = { draw: Sprite(self.right) };
       } else if (thing.vel.x < 0) {
-        thing.colorScheme = { draw: Sprite('enemyl') };
+        thing.colorScheme = { draw: Sprite(self.left) };
       }
       if (game.numberDistance(player.x, thing.x) < game.width * 2 && game.numberDistance(player.y, thing.y) < game.height * 2)
         thing.to(player.x, 0, 100, 0);
@@ -196,7 +206,7 @@ function sugarcane(x, bottom) {
     background: Sprite('sugar-cane'),
     width: 200,
     height: 120,
-    custom: {
+    custom: { 
       physics: true,
       pass: true,
       funcText: 'Collect Sugar',
@@ -207,6 +217,51 @@ function sugarcane(x, bottom) {
     }
   })
 }
+let level9 = new Scene((x, y, ground) => {
+  new Door('Paradise', Sprite('door-back'), x-975, ground.top - 35, paradise);
+  new Enemy(x + 300, ground.top, 70, 70, 100);
+  new Enemy(x + 500, ground.top, 70, 70, 100);
+  new Enemy(x + 300, ground.top, 70, 70, 100);
+  new Enemy(x - 200, ground.top, 70, 70, 100);
+  new Enemy(x - 600, ground.top, 70, 70, 100);
+  new Enemy(x + 200, ground.top, 300, 300, 800, 'e2l', 'e2r');
+  Chest(x + 700, ground.top, [ghost.copy(0)]);
+}, 2000, -975);
+let level8 = new Scene((x, y, ground) => {
+  new Door('Paradise', Sprite('door-back'), x-975, ground.top - 35, paradise);
+  cow(x-200, ground.top);
+  Chest(x, ground.top, [Aji.copy(0)]);
+  new Enemy(x + 600, ground.top, 30, 30, 80);
+  new Door('Level 9', Sprite('door'), x + 310, ground.top - 25, level9)
+  new Enemy(x + 500, ground.top, 60, 60, 210);
+  new Enemy(x + 420, ground.top, 60, 60, 210);
+  new Enemy(x + 300, ground.top, 100, 100, 400);
+  new Enemy(x + 280, ground.top, 20, 20, 210);
+  new Enemy(x + 220, ground.top, 50, 50, 120)
+  new Enemy(x + 200, ground.top, 60, 60, 210);
+  new Enemy(x + 180, ground.top, 60, 60, 210);
+  new Enemy(x + 160, ground.top, 60, 60, 210);
+  new Enemy(x + 120, ground.top, 60, 60, 210);
+  new Enemy(x + 100, ground.top, 60, 60, 210);
+}, 2000, -975)
+let level7 = new Scene((x, y, ground) => {
+  new Door('Paradise', Sprite('door-back'), x-475, ground.top - 35, paradise);
+  new Enemy(x - 280, ground.top, 20, 20, 210);
+  new Enemy(x - 220, ground.top, 50, 50, 120)
+  new Enemy(x - 200, ground.top, 60, 60, 210);
+  new Enemy(x - 180, ground.top, 60, 60, 210);
+  new Enemy(x - 160, ground.top, 60, 60, 210);
+  new Enemy(x - 120, ground.top, 60, 60, 210);
+  new Enemy(x - 100, ground.top, 60, 60, 210);
+  new Door('Level 8', Sprite('door'), x+50, ground.top - 35, level8);
+  Chest(x + 100, ground.top, [ghost.copy(10)]);
+  Chest(x + 200, ground.top, [LemonJuice.copy()]);
+  new game.Thing({x:x,bottom:ground.top,width:70,height:100, custom:{physics:true}, background:'red'});
+  new Enemy(x + 300, ground.top, 100, 100, 400);
+  new Enemy(x + 420, ground.top, 60, 60, 210);
+  new Enemy(x + 500, ground.top, 60, 60, 210);
+  new EnemyWall(x - 400, ground.top);
+}, 1000, -475)
 let level6 = new Scene((x, y, ground) => {
   new Door('Paradise', Sprite('door-back'), x, ground.top - 35, paradise);
   new Enemy(x-1000, ground.top, 100, 100, 200);
@@ -217,12 +272,14 @@ let level6 = new Scene((x, y, ground) => {
   new Enemy(x+600, ground.top, 20, 20, 75);
   new Enemy(x-400, ground.top, 200, 200, 250);
   new Enemy(x+400, ground.top, 200, 200, 250);
+  new Chest(x+500, ground.top, [ghost.copy(10)]);
+  new Door('Level 7', Sprite('door'), x+600, ground.top-25, level7);
 }, 2000);
 let level5 = new Scene((x, y, ground) => {
   new Door('Paradise', Sprite('door-back'), x, ground.top - 35, paradise);
   new Enemy(x-400, ground.top, 100, 100, 200);
   new Enemy(x+400, ground.top, 100, 100, 200);
-  new Chest(x+425, ground.top, [Aji.copy(0), ghost.copy(10)]);
+  new Chest(x+425, ground.top, [Aji.copy(0)]);
   new Door('Level 6', Sprite('door'), x+475, ground.top - 35, level6)
 }, 1000)
 let paradise = new Scene((x, y, ground) => {
@@ -239,7 +296,7 @@ let level4 = new Scene((x, y, ground) => {
   new Message(x - 950, ground.bottom, 'The door home is at the end');
   new Enemy(x - 200, ground.top, 50, 50, 120);
   new Enemy(x - 220, ground.top, 40, 40, 100);
-  new Enemy(x - 240, ground.top, 200, 200, 1000);
+  new Enemy(x - 240, ground.top, 200, 200, 1000, 'e2l', 'e2r');
   Chest(x + 200, ground.top, [Aji.copy(0)]);
   new Door('Paradise', Sprite('door'), x + 100, ground.top - 35, paradise);
   new Door('Home', Sprite('door-back'), x, ground.top - 35, mainScene);
@@ -263,7 +320,7 @@ let level2 = new Scene((x, y, ground) => {
 }, 2000, -800)
 let otherland = new Scene((x, y, ground) => {
   new Door('Home', Sprite('door-back'), x, ground.top - 35, mainScene);
-  new Enemy(600 + x, ground.top, 50, 50);
+  new Enemy(600 + x, ground.top, 50, 50, 100);
   EnemyWall(x + 200, ground.top);
   Chest(x + 700, ground.top, [jalapeno.copy()]);
   new Door('Level 2', Sprite('door'), x + 600, ground.top - 35, level2);
@@ -399,7 +456,6 @@ KEYS.bindKeyHold(['a', 'ArrowLeft'], e => { player.vel.x = -200; playerDir = "le
 KEYS.bindKeyHold(['d', 'ArrowRight'], e => { player.vel.x = 200; playerDir = "right"; })
 let lastShot = 0;
 function Shoot() {
-  console.log('SHOOT');
   if (player.weapon && Date.now()-lastShot>10) {
     lastShot = Date.now();
     player.weapon.use();
@@ -501,4 +557,4 @@ setInterval(() => {
   document.body.style.webkitTransform = 'scale(1)';
   document.body.style.msTransform = 'scale(1)';
   document.body.style.transform = 'scale(1)';
-}, 100)
+}, 100);
